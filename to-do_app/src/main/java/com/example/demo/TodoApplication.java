e82e8a7177fb3ddd5b87dc84de049011c1ee0ed7
nungo <nued65@gmail.com>
2025-09-10 14:10:21 +0300

    feat:add rest api to print Hello world in browser

--- to-do_app/src/main/java/com/example/demo/TodoApplication.java
+++ to-do_app/src/main/java/com/example/demo/TodoApplication.java
@@ -2,10 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class TodoApplication {
 
+	@GetMapping("/hello")
+	public String getMethodName(@RequestParam(defaultValue="world", value="name") String param) {
+		return String.format("Hello %s", param);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
 	}
