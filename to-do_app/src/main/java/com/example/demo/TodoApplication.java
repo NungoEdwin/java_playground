239ef1814f0a0dd191424a8bffa267a25bc2598d
nungo <nued65@gmail.com>
2025-09-10 15:35:16 +0300

    feat:add another rest controller which returns a json output

--- to-do_app/src/main/java/com/example/demo/TodoApplication.java
+++ to-do_app/src/main/java/com/example/demo/TodoApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class TodoApplication {
+	private String template = "Hello, %s!";
+	private AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/hello")
 	public String getMethodName(@RequestParam(defaultValue="world", value="name") String param) {
 		return String.format("Hello %s", param);
 	}
+	
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+	//record is a DTO(POJO) like data struct for immutable data 
+	public record Greeting(long is,String content){}
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
