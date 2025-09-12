9c0c0fcf7e2ed94cbe66b5296a56b216033d661c
nungo <nued65@gmail.com>
2025-09-12 16:19:28 +0300

    chore:cleaning up redundant imports

--- to-do_app/src/main/java/com/example/demo/repositories/ProductRepo.java
+++ to-do_app/src/main/java/com/example/demo/repositories/ProductRepo.java
@@ -1,8 +1,6 @@
 package com.example.demo.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.demo.models.Product;
 
-public interface ProductRepo extends JpaRepository<Product,Long> {
-    
-}
+
+public interface ProductRepo extends JpaRepository<Product,Long> {}
